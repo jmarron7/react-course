8d55c8a76920684f9a07aa26410876e38163bb61
Jesus Marron <jesusmarron7@gmail.com>
2023-03-27 16:41:49 -0500

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London"];
+  let items = ["New York", "San Francisco", "Tokyo", "London"];
+  //items = [];
 
   return (
     // <> Fragment same as <Fragment>
     <>
       <h1>LIST</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
