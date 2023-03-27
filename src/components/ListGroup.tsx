f6471ce35273ce13e98d8e6c293e513519f31060
Jesus Marron <jesusmarron7@gmail.com>
2023-03-27 14:57:48 -0500

    fragments

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+function ListGroup() {
+  return (
+    // <> Fragment same as <Fragment>
+    <>
+      <h1>LIST</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Item 1</li>
+        <li className="list-group-item">Item 2</li>
+        <li className="list-group-item">Item 3</li>
+        <li className="list-group-item">Item 4</li>
+        <li className="list-group-item">Item 5</li>
+        <li className="list-group-item">Item 6</li>
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
