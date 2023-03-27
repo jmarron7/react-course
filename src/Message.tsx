e0dc2d1d6322d80a2171af88c319482ee9ff06a2
Jesus Marron <jesusmarron7@gmail.com>
2023-03-27 16:55:58 -0500

    handling events. managing state

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,7 @@
-
 function Message() {
-    const name = "Jesus";
-    if (name)
-        return <h1>Hello {name}</h1>
-    return <h1>Hello World</h1>
+  const name = "Jesus";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
 }
 
 export default Message;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London"];
-  //items = [];
-
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     // <> Fragment same as <Fragment>
     <>
       <h1>LIST</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
