6afc5109fc6bacf55933a26ed339ea84e1ae4897
Jesus Marron <jesusmarron7@gmail.com>
2023-03-28 19:13:04 -0500

    initial form created

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Button from "./components/Button";
 import ListGroup from "./components/ListGroup/";
 import { DiGithub } from "react-icons/di";
 import Like from "./components/Like";
-
+import Form from "./components/Form";
 function App() {
   const items = ["Miami", "New York", "Los Angeles"];
   return (
     <>
-      <Like onClick={() => console.log("Liked")} />
+      <Form />
     </>
   );
 }
